b7c2ef98dbdc443c9e182806840e663c92e864ea
Samuel MARLHENS <11817371+smarlhens@users.noreply.github.com>
2022-06-16 19:01:38 +0200

    Initial commit

---
+++ src/myapplication/main.py
@@ -0,0 +1,10 @@
+def hello_world():
+    return "Hello World"
+
+
+def main():
+    print(hello_world())
+
+
+if __name__ == "__main__":
+    main()
---
+++ tests/test_myapplication.py
@@ -0,0 +1,5 @@
+from myapplication.main import hello_world
+
+
+def test_hello_world():
+    assert hello_world() == "Hello World"
