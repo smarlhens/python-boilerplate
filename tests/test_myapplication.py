5fd05ddc242927bb82578b13988e5b2d56734401
Samuel MARLHENS <samuel.marlhens@upciti.com>
2024-05-29 14:02:52 +0200

    chore: replace black by ruff format

--- tests/test_myapplication.py
+++ tests/test_myapplication.py
@@ -2,4 +2,5 @@ from myapplication.main import hello_world
 
 
 def test_hello_world():
-    assert hello_world() == "Hello World"
+    if hello_world() != "Hello World":
+        raise ValueError('Expected value to be "Hello World"')
