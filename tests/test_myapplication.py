59d677306ed284625713301f16624e6303df8466
Samuel MARLHENS <samuel.marlhens@upciti.com>
2024-06-20 13:05:10 +0200

    chore: fix missing return types

--- tests/test_myapplication.py
+++ tests/test_myapplication.py
@@ -1,6 +1,6 @@
 from myapplication.main import hello_world
 
 
-def test_hello_world():
+def test_hello_world() -> None:
     if hello_world() != "Hello World":
         raise ValueError('Expected value to be "Hello World"')
