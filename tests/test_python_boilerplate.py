00e6cbababaa1229f676bb655f4016bd700a0694
Samuel MARLHENS <samuel.marlhens@upciti.com>
2026-07-27 14:55:25 +0200

    chore(ruff): dedupe extend-select against v0.16 defaults, widen rule set

--- tests/test_python_boilerplate.py
+++ tests/test_python_boilerplate.py
@@ -2,5 +2,4 @@ from python_boilerplate.main import hello_world
 
 
 def test_hello_world() -> None:
-    if hello_world() != "Hello World":
-        raise ValueError('Expected value to be "Hello World"')
+    assert hello_world() == "Hello World"
